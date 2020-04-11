785cdda1bb8cf20355c2f9c07e9c168ff1b8ea20
Nikita Galkov <tyler_1991@mail.ru>
2020-04-11 18:45:21 +0300

    App Store publish

--- Bird Hero/GameViewController.swift
+++ Bird Hero/GameViewController.swift
@@ -21,8 +21,8 @@ class GameViewController: UIViewController {
         
         let scene = GameScene(size: CGSize(width: 2048, height: 1536))
         let skView = self.view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        //skView.showsFPS = true
+        //skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
         scene.scaleMode = .aspectFill
         skView.presentScene(scene)
